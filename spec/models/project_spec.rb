815973cad21f65aba5ec53b4afebda82a543e20f
v_shmyhlo <aion.stu@gmail.com>
2015-01-09 21:36:39 +0200

    models specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,26 +1,33 @@
 require 'spec_helper'
 
 describe Project do
-    # pending "add some examples to (or delete) #{__FILE__}"
   subject(:project) { FactoryGirl.build(:project) }
 
   it "has valid name" do
     project.name = 'some name'
-    expect(project.valid?).to be_true
+    expect(project.valid?).to be true
   end
 
   it "is invalid because of short name" do
     project.name = 'do'
-    expect(project.invalid?).to be_true
+    expect(project.invalid?).to be true
   end
 
   it "is invalid because of long name" do
     project.name = 'a'*81
-    expect(project.invalid?).to be_true
+    expect(project.invalid?).to be true
   end
 
   it "is invalid because of empty name" do
     project.name = ''
-    expect(project.invalid?).to be_true
+    expect(project.invalid?).to be true
+  end
+
+  it "should be destroyed with it's tasks" do
+    project.tasks.build([{},{},{}])
+    project.save
+    ids = project.tasks.map &:id
+    project.destroy
+    expect(Task.where(id: ids).count).to eq(0)
   end
 end
