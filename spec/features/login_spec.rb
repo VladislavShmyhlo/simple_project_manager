b087542fa56b331fcd708c568c3ac83056d237ec
v_shmyhlo <aion.stu@gmail.com>
2015-01-26 01:38:25 +0200

    fixes

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
-feature "loin", js: true do
-  scenario "user login" do
+feature "login", js: true do
+  scenario "user logs in" do
     visit '/'
     fill_in "user_email", with: "root@root.root"
     fill_in "user_password", with: "rootroot"
     click_on "Log in"
 
     expect(page).to have_content("project")
-    # expect(page).to have_content("Baked Brussel Sprouts")
   end
 end
