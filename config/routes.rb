b9dcfa4a0a390eac0d20c422dcf49438ca86a605
v_shmyhlo <aion.stu@gmail.com>
2015-01-25 14:12:04 +0200

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # TODO: this should be more DRY
 
