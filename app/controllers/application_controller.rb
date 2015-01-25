2f8d592e9bae9d67b23b5fae80eb8a6d2c67839b
v_shmyhlo <aion.stu@gmail.com>
2015-01-25 15:36:32 +0200

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource_or_scope)
-    if request.env['omniauth.origin']
-      request.env['omniauth.origin']
-    end
-  end
+  # def after_sign_in_path_for(resource_or_scope)
+  #   if request.env['omniauth.origin']
+  #     request.env['omniauth.origin']
+  #   end
+  # end
 
   private
 
-  def redirect_to_home
-    unless request.format == 'application/json'
-      redirect_to root_path
-    end
-  end
+  # def redirect_to_home
+  #   unless request.format == 'application/json'
+  #     redirect_to root_path
+  #   end
+  # end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,11 +4,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect root_path, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+
+      render text: @user.errors.to_a.to_s << '\n' << request.env["omniauth.auth"]
+
+
+      # redirect_to new_user_registration_url
     end
   end
 end
