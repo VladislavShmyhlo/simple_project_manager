5a7e4726586ac5f151159540b7a5a0802ec45166
v_shmyhlo <aion.stu@gmail.com>
2015-01-25 15:03:05 +0200

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,12 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
-  # before_action :redirect_to_home
 
-  # def after_sign_in_path_for(resource_or_scope)
-  #   if request.env['omniauth.origin']
-  #     request.env['omniauth.origin']
-  #   end
-  # end
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
 
   private
 
