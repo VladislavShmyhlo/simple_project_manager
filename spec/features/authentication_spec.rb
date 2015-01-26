ec6825b973474cf97e28c9e5fabfb10409db6c0b
v_shmyhlo <aion.stu@gmail.com>
2015-01-26 22:41:18 +0200

    auth specs

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,16 +2,33 @@ require 'spec_helper'
 
 feature "authentication", js: true do
   let(:user) { FactoryGirl.create :user }
+
   background do
     user.confirmed_at = Time.now
     user.save
   end
+
   scenario "user logs in" do
     visit '/'
     fill_in "user_email", with: user.email
     fill_in "user_password", with: user.password
     click_on "Log in"
 
-    expect(page).to have_content("create new project")
+    expect(page).to have_content("create a project")
+  end
+
+  context "when logged in user" do
+    background do
+      visit '/'
+      fill_in "user_email", with: user.email
+      fill_in "user_password", with: user.password
+      click_on "Log in"
+    end
+
+    scenario "signs out" do
+      click_on "logout"
+
+      expect(page).to have_content("log in")
+    end
   end
 end
