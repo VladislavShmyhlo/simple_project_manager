a4005221e1e46a9fb8f92d92b0c57dd53dbe78cd
v_shmyhlo <aion.stu@gmail.com>
2015-01-27 00:19:59 +0200

    auth specs

--- spec/features/project_management_spec.rb
+++ spec/features/project_management_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
-feature 'user manages projects', js: true do
+feature 'project management', js: true do
   let(:user) { FactoryGirl.create :user }
+  let(:project_name) { 'project name' }
 
   background do
     user.confirmed_at = Time.now
@@ -13,20 +14,33 @@ feature 'user manages projects', js: true do
     click_on 'Log in'
   end
 
-  scenario 'user creates new project' do
-    find('.new-project-button button').click
-    within '.new-project-form' do
-      fill_in :name, with: 'new project name'
-      find('button.save').click
+  feature 'project creation' do
+    background do
+      find('.new-project-button button').click
     end
-    expect(find('.project .name')).to have_content 'new project name'
+
+    scenario 'user creates new project' do
+      within '.new-project-form' do
+        fill_in :name, with: project_name
+        find('button.save').click
+      end
+      expect(find('.project .name')).to have_content project_name
+    end
+
+    scenario 'user fails to create new project due to validation' do
+      within '.new-project-form' do
+        fill_in :name, with: ' '
+        find('button.save').click
+      end
+      expect(page).to have_no_selector '.project'
+    end
   end
 
   context 'when user has project' do
     background do
       find('.new-project-button button').click
       within '.new-project-form' do
-        fill_in 'name', with: 'new project name'
+        fill_in 'name', with: project_name
         find('button.save').click
       end
     end
@@ -36,14 +50,29 @@ feature 'user manages projects', js: true do
       expect(page).to have_no_selector '.project'
     end
 
-    scenario 'user changes project name' do
-      expect {
-        find('.project .edit').click
-        within '.project-name-form' do
-          fill_in :name, with: 'new name'
-          find('button.save').click
-        end
-      }.to change{ find('.project .name').text }.from('new project name').to('new name')
+    feature 'project renaming' do
+      let(:new_valid_name) { 'new name' }
+      let(:new_invalid_name) { ' ' }
+      
+      scenario 'user changes project name' do
+        expect {
+          find('.project .edit').click
+          within '.project-name-form' do
+            fill_in :name, with: new_valid_name
+            find('button.save').click
+          end
+        }.to change{ find('.project .name').text }.from(project_name).to(new_valid_name)
+      end
+
+      scenario 'user fails to change project name due to validation' do
+        expect {
+          find('.project .edit').click
+          within '.project-name-form' do
+            fill_in :name, with: new_invalid_name
+            find('button.save').click
+          end
+        }.to_not change{ find('.project .name').text }.from(project_name).to(new_invalid_name)
+      end
     end
   end
 end
