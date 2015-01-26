cc3a35682780e2666dec92e6feb9d1f02abecb3b
v_shmyhlo <aion.stu@gmail.com>
2015-01-27 01:02:21 +0200

    auth specs

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -14,10 +14,12 @@ feature "authentication", js: true do
     fill_in "user_password", with: user.password
     click_on "Log in"
 
-    expect(page).to have_content(/create a project/i)
+    expect(page).to have_button(/add new project/i)
   end
 
   context "when logged in user" do
+    let(:new_password) { 'qwerty' }
+
     background do
       visit '/'
       fill_in "user_email", with: user.email
@@ -27,8 +29,17 @@ feature "authentication", js: true do
 
     scenario "signs out" do
       click_on "logout"
-
       expect(page).to have_content(/signed out successfully/i)
+    end
+
+    scenario 'changes password' do
+      visit 'users/edit'
+      fill_in 'user_password', with: new_password
+      fill_in 'user_password_confirmation', with: new_password
+      fill_in 'user_current_password', with: user.password
+      click_on /update/i
+      expect(page).to have_button(/add new project/i)
+      expect(current_path).to eq('/')
     end
   end
 end
