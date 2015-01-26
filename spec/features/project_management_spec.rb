0a97bb9e6890cb21afaeb15442dcb6897a0d1f64
v_shmyhlo <aion.stu@gmail.com>
2015-01-27 00:04:30 +0200

    auth specs

--- spec/features/project_management_spec.rb
+++ spec/features/project_management_spec.rb
@@ -16,7 +16,7 @@ feature 'user manages projects', js: true do
   scenario 'user creates new project' do
     find('.new-project-button button').click
     within '.new-project-form' do
-      fill_in 'name', with: 'new project name'
+      fill_in :name, with: 'new project name'
       find('button.save').click
     end
     expect(find('.project .name')).to have_content 'new project name'
@@ -34,6 +34,16 @@ feature 'user manages projects', js: true do
     scenario 'user deletes project' do
       find('.project .remove').click
       expect(page).to have_no_selector '.project'
+    end
+
+    scenario 'user changes project name' do
+      find('.project .edit').click
+      expect {
+        within '.project-name-form' do
+        fill_in :name, with: 'new name'
+        find('button.save').click
+        end
+      }.to change{ find('.project .name').text }.from('new project name').to('new name')
     end
   end
 end
