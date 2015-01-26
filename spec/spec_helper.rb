99393ea3d0fa773d73eaa6d972130eddc650ab17
v_shmyhlo <aion.stu@gmail.com>
2015-01-26 21:47:41 +0200

    auth spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,12 +28,20 @@ RSpec.configure do |config|
 
 
 
+
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
 
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 
   config.before(:suite) do
