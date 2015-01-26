d5ec14f4612c48f6a7b58d5654cc787e3e225efb
v_shmyhlo <aion.stu@gmail.com>
2015-01-26 22:04:14 +0200

    small fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,30 +26,12 @@ RSpec.configure do |config|
 
   Capybara.default_driver = :selenium
 
-
-
-
-  config.use_transactional_fixtures = false
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
-  # config.before(:each) do
-  #   DatabaseCleaner.strategy = :transaction
-  # end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
