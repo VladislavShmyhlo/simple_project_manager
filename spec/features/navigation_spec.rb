52ff2a561681dc9cfdd214342a420af34e2dbd0b
v_shmyhlo <aion.stu@gmail.com>
2015-01-27 01:49:05 +0200

    auth specs

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -25,7 +25,7 @@ feature 'navigation', js: true do
 
     scenario 'navigates to single project page' do
       find('.created-project .name a').click
-      expect(current_path).to match(/\A\/projects\/\d+\z/)
+      expect(page.execute_script("location.hash")).to match(/\A\#\/projects\/\d+\z/)
     end
   end
 end
