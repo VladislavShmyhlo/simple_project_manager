2c9ac28d882e79940e709939b0dd0f76349be66f
v_shmyhlo <aion.stu@gmail.com>
2015-01-26 22:23:01 +0200

    fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,8 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    # DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
   end
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
