2e25718c7e2e290433bf99d3960882bb293700b4
v_shmyhlo <aion.stu@gmail.com>
2015-01-27 23:32:46 +0200

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
   # Print deprecation notices to the Rails logger.
