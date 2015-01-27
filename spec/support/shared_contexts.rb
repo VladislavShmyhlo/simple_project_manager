6d09402320e9f3eb469697c0242dc79f5540b16c
v_shmyhlo <aion.stu@gmail.com>
2015-01-28 01:07:38 +0200

    specs

--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -11,3 +11,13 @@ shared_context 'user logged in' do
     click_on 'Log in'
   end
 end
+
+shared_context 'valid session' do
+  let(:user) { FactoryGirl.create :user }
+
+  before :each do
+    user.confirmed_at = Time.now
+    user.save
+    sign_in user
+  end
+end
