bbca5e92896f1a2d25e23dbe9a0c902034317860
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 00:24:36 +0200

    specs

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -20,8 +20,8 @@ feature 'task management', js: true do
       scenario 'user fails to create task due to validation' do
         within '.new-task-form' do
           fill_in :description, with: invalid_description
-          expect.to have_button('Add Task', disabled: true)
         end
+        expect(find('.new-task-form')).to have_button('Add Task', disabled: true)
       end
     end
   end
@@ -39,7 +39,7 @@ feature 'task management', js: true do
         expect {
           find('.task .edit').click
           within '.task-description-form' do
-            fill_in 'textarea', with: new_description
+            fill_in 'description', with: new_description
             find('button.save').click
           end
         }.to change{find('.task .description').text}.from(description).to(new_description)
@@ -48,9 +48,9 @@ feature 'task management', js: true do
       scenario 'user fails to change task description due to validation' do
         find('.task .edit').click
         within '.task-description-form' do
-          fill_in 'textarea', with: invalid_description
-          expect.to have_button('.save', disabled: true)
+          fill_in 'description', with: invalid_description
         end
+        expect(find('.task-description-form')).to have_button('.save', disabled: true)
       end
     end
   end
