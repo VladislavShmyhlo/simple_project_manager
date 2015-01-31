ed7d5b100c9af507a198d68ebbcc54f7bb5326bd
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 14:33:43 +0200

    cleaning

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -3,15 +3,7 @@ require 'spec_helper'
 describe Users::OmniauthCallbacksController do
     pending 'need to be implemented'
 
-
 # it "routes to users/omniauth_callbacks#facebook" do
 #       get("/users/auth/facebook/callback").should route_to("users/omniauth_callbacks#facebook", action: 'facebook')
 #     end
-
 end
-
-
-
-
-
-
