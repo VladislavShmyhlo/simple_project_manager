22f51176cf79290aebbd8de19a28cd736f05295a
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 03:23:44 +0200

    specs

--- spec/features/comment_management_spec.rb
+++ spec/features/comment_management_spec.rb
@@ -18,7 +18,7 @@ feature 'comment management', js: true do
     find('.task .description').click
     within '.new-comment-form' do
       fill_in :body, with: ' '
-      click_on 'add new comment'
+      click_on 'add new comment', disabled: true
     end
     expect(page).to have_no_selector('.comments-list .comment')
   end
@@ -26,6 +26,20 @@ feature 'comment management', js: true do
   context 'when user has comment' do
     include_context 'logged in user has comment'
 
+    background do
+      find('.task .description').click
+    end
 
+    scenario 'user deletes comment' do
+      find('.comment-remove').click
+      expect(page).to have_no_selector('.comments-list .comment')
+    end
+
+    scenario 'user uploads file' do
+      within 'attachmentForm' do
+        attach_file(:file, File.join(Rails.root, 'config.ru'))
+      end
+      expect(find('.attachment .name')).to have_content('config.ru')
+    end
   end
 end
