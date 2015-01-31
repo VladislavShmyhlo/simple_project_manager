a8928bfbeb8d4e0331897a95f36ecffcfd75fd8f
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 10:10:25 +0200

    specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,12 @@ describe User do
     expect(user).to_not be_valid
   end
 
+  it 'pases with provider is set and empty email' do
+    user.email = " "
+    user.provider = 'facebook'
+    expect(user).to be_valid
+  end
+
   it 'fails with bad email' do
     user.email = "random string"
     expect(user).to_not be_valid
@@ -23,13 +29,20 @@ describe User do
     expect(new_user).to_not be_valid
   end
 
-  it 'fails with short password' do
-    user = User.new(email: 'test@example.com', password: 'pass', password_confirmation: 'pass')
+  it 'fails with empty password' do
+    user = User.new(email: 'test@example.com', password: ' ')
     expect(user).to_not be_valid
   end
 
-  it 'fails with unconfirmed password' do
-    user = User.new(email: 'test@example.com', password: 'password')
+  it 'fails with short password' do
+    user = User.new(email: 'test@example.com', password: 'pass')
+    expect(user).to_not be_valid
+  end
+
+  it 'fails with bad password confirmation' do
+    user = User.new(email: 'test@example.com',
+                    password: 'password',
+                    password_confirmation: 'pass')
     expect(user).to_not be_valid
   end
 end
