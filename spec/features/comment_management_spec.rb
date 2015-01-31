ef4ea46e346f46610b5be3ecad30c6a449145600
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 03:59:24 +0200

    specs

--- spec/features/comment_management_spec.rb
+++ spec/features/comment_management_spec.rb
@@ -54,7 +54,7 @@ feature 'comment management', js: true do
 
     scenario 'user deletes attachment' do
       find('.attachment-remove').click
-      expect(find('.attachments-list')).to have_no_selector('.attachment')
+      expect(find('.attachments-list', visible: false)).to have_no_selector('.attachment')
     end
   end
 end
