2174479bb999c64b4a546c143fa0f3762772ff17
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 03:25:16 +0200

    specs

--- spec/features/comment_management_spec.rb
+++ spec/features/comment_management_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
 feature 'comment management', js: true do
-  include_context 'logged in user has task'
+  context 'when user has task' do
+    include_context 'logged in user has task'
 
-  let(:body) { 'my new comment' }
+    let(:body) { 'my new comment' }
 
-  scenario 'creates comment' do
-    find('.task .description').click
-    within '.new-comment-form' do
-      fill_in :body, with: body
-      click_on 'add new comment'
+    scenario 'creates comment' do
+      find('.task .description').click
+      within '.new-comment-form' do
+        fill_in :body, with: body
+        click_on 'add new comment'
+      end
+      expect(find('.comments-list .comment')).to have_content(body)
     end
-    expect(find('.comments-list .comment')).to have_content(body)
-  end
 
-  scenario 'fails to create comment due to validation' do
-    find('.task .description').click
-    within '.new-comment-form' do
-      fill_in :body, with: ' '
-      click_on 'add new comment', disabled: true
+    scenario 'fails to create comment due to validation' do
+      find('.task .description').click
+      within '.new-comment-form' do
+        fill_in :body, with: ' '
+        click_on 'add new comment', disabled: true
+      end
+      expect(page).to have_no_selector('.comments-list .comment')
     end
-    expect(page).to have_no_selector('.comments-list .comment')
   end
 
   context 'when user has comment' do
