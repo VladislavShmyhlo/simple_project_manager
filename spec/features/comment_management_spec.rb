9209601af40ff538f3d45593403cd41a34de0b09
v_shmyhlo <aion.stu@gmail.com>
2015-01-31 03:15:57 +0200

    specs

--- spec/features/comment_management_spec.rb
+++ spec/features/comment_management_spec.rb
@@ -14,7 +14,18 @@ feature 'comment management', js: true do
     expect(find('.comments-list .comment')).to have_content(body)
   end
 
+  scenario 'fails to create comment due to validation' do
+    find('.task .description').click
+    within '.new-comment-form' do
+      fill_in :body, with: ' '
+      click_on 'add new comment'
+    end
+    expect(page).to have_no_selector('.comments-list .comment')
+  end
+
   context 'when user has comment' do
     include_context 'logged in user has comment'
+
+
   end
 end
